9d1f4fb7576b81ff5ad846b75285ea3ee17ab55e
Natalia Fernandez <91369340+NataliaFdez@users.noreply.github.com>
2021-12-19 23:29:46 +0100

    Proyecto actualizado

--- src/main.py
+++ src/main.py
@@ -3,63 +3,66 @@ import numpy as np
 import pandas as pd
 import utils
 import constants
+import string
 
 
 # REVANCHA
 
 revancha = True
 while revancha:
-  name_user = utils.welcome()
-  player = utils.User()
-  pc = utils.User() 
+    name_user = utils.welcome()
+    player = utils.User()
+    pc = utils.User()
+    
+    ocean = utils.ocean_dimension()
+    fleet = utils.fleet()
 
-  # random_boats()
-  player_coordinates = [('A', 10)]
-  pc_coordinates = [('C', 1)]
 
-  utils.insert_boats(player_coordinates, pc_coordinates, player, pc)
+    # RANDOM BOATS
+    player_coordinates = player.boats_generator(fleet, ocean)
+    pc_coordinates = pc.boats_generator(fleet, ocean)
 
-  # TURNS
-  play = True
-  while play:
-      utils.shoot_player(pc_coordinates, player, pc)
-      print(f'\n{name_user} estos son tus disparos.', player.shots, f'{name_user} estos son tus barcos.', player.boats, 'PC estos son tus disparos.', pc.shots, 'PC estos son tus barcos.', pc.boats, sep = '\n\n')
+    # INSERT BOATS
+    utils.insert_boats(player_coordinates, pc_coordinates, player, pc)
 
-      if len(player_coordinates) == 0 or len(pc_coordinates) == 0:
-          break
+    # TURNS
+    play = True
+    while play:
+        utils.shoot_player(pc_coordinates, player, pc)
+        print(f'\n{name_user} estos son tus disparos.', player.shots, f'{name_user} estos son tus barcos.', player.boats, sep = '\n\n')
 
-      utils.shoot_pc(player_coordinates, pc, player)
-      print(f'\n{name_user} estos son tus disparos.', player.shots, f'{name_user} estos son tus barcos.', player.boats, 'PC estos son tus disparos.', pc.shots, 'PC estos son tus barcos.', pc.boats, sep = '\n\n')
+        if len(player_coordinates) == 0 or len(pc_coordinates) == 0:
+            break
 
-      if len(player_coordinates) == 0 or len(pc_coordinates) == 0:
-          break
+        utils.shoot_pc(player_coordinates, pc, player)
+        print('PC estos son tus disparos.', pc.shots, 'PC estos son tus barcos.', pc.boats, sep = '\n\n')
 
-  # END OF GAME
-  if len(player_coordinates) == 0:
-    print(constants.GAME_OVER)
-    pass
+        if len(player_coordinates) == 0 or len(pc_coordinates) == 0:
+            break
 
-  if len(pc_coordinates) == 0:
+    # END OF GAME
+    if len(player_coordinates) == 0:
+        print(constants.GAME_OVER)
+        pass
+
+    if len(pc_coordinates) == 0:
         print(constants.LEVEL_UP)
-                                                                 
-  pass
-  
-  respuesta = input(f'{name_user} quieres jugar otra partida? Escribe SI ó NO.').upper()
+        pass
 
-  while respuesta != 'SI' and respuesta != 'NO':
-    print(respuesta)
-    print('No te he entendido')
-    respuesta = input('Echamos otra partida? Escribe SI ó NO.').upper()
+    respuesta = input(f'{name_user} quieres jugar otra partida? Escribe SI ó NO.').upper()
 
+    while respuesta != 'SI' and respuesta != 'NO':
+        print(respuesta)
+        print('No te he entendido')
+        respuesta = input('Echamos otra partida? Escribe SI ó NO.').upper()
 
-  if respuesta == 'SI':
-    revancha = True
-    player.clear()
-    pc.clear()
-    print('¡EMPECEMOS!')
+        if respuesta == 'SI':
+            revancha = True
+            player.clear()
+            pc.clear()
+            print('¡EMPECEMOS!')
 
-  elif respuesta == 'NO':
-    revancha = False
-    print('Fin del juego')
-    utils.despedida()
-
+        elif respuesta == 'NO':
+            revancha = False
+            print('Fin del juego')
+            utils.despedida()
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 import time
 import sys
 import constants
+import string
+
+
 
 # WELCOME
 
@@ -29,6 +32,65 @@ def welcome():
     delay_print('¡Al ataque!')
     return name_user
 
+# BUILDING OCEAN
+
+def ocean_dimension():
+    
+    while True:
+        try:
+            size = int(input('\n\nIntroduce el tamaño del lado del tablero que quieras usar (en número):'))
+            break
+        except ValueError: 
+            continue
+
+    if size <= 0:
+        print('¿Un océano de antimateria? Mejor juguemos con el tablero clásico de 10x10...')
+        size = 10
+        col_index = list(string.ascii_uppercase)[:size] 
+
+    elif size < 10:
+        print('¿Eso es un océano o un embalse? Mejor juguemos con el tablero clásico de 10x10...')
+        size = 10
+        col_index = list(string.ascii_uppercase)[:size]
+
+    elif size <= 26:
+        col_index = list(string.ascii_uppercase)[:size] 
+
+    elif 26 < size <= 676:
+        duoble_alphabet = list(itertools.product(string.ascii_uppercase,
+            repeat = 2))
+        col_index = list(string.ascii_uppercase) + [(str(letters[0]) + str(letters[1])) for letters in duoble_alphabet]
+    
+    elif size <= 19000:
+        triple_alphabet = list(itertools.product(string.ascii_uppercase,
+            repeat = 3))
+        col_index = list(string.ascii_uppercase) + [(str(letters[0]) + str(letters[1]) + str(letters[2])) for letters in triple_alphabet]
+    
+    elif size > 19000:
+        print('''
+            Según Wikipedia:
+            Océano
+            Cuerpo de agua
+            Superficie:          361 000 000 km²
+            Volumen:           1 300 000 000 km³
+            Profundidad Media:         3 900 m
+    
+            Si cada casilla equivale a 1km², has
+            creado un tablero más grande que toda
+            la lámina de agua del planeta.
+    
+            Si te lo estabas preguntando, Challenger Deep
+            está en la casilla 11°22.4′N 142°35.5′E.
+            
+            Mejor juguemos con el tablero clásico de 10x10...
+        ''')
+        size = 10
+        col_index = list(string.ascii_uppercase)[:size] 
+    
+    ocean = (size + 1, col_index)
+    
+    return ocean
+
 
 
 # BUILDING USERS
@@ -44,13 +106,94 @@ class User:
 
         return None
     
+    def boats_generator(self, fleet, ocean):
+        coordinates = []
+        fleet.sort(reverse = True)
+
+        for ship in fleet:
+            boat_in = False
+            attemps = 0
+            
+            if attemps == 100:
+                print('Uno de los barcos se ha averiado y no ha podido desplegarse.')
+            
+            while boat_in == False:
+                bow = np.random.randint(0, ocean[0])
+                course = ocean[1].index(np.random.choice(ocean[1]))
+                way = np.random.choice(list('NSEW'))
+                
+                if way == 'N' or way == 'E':
+                    stern = bow + ship
+                    way = 1
+                    zone = self.boats.iloc[bow: stern: way, course]
+
+                    # Check if zone inside ocean:
+                    condition_1 = bow in range(self.shots.shape[0])
+                    condition_2 = stern in range(self.shots.shape[0])
+                    
+                    # Check if zone is empty:
+                    condition_3 = True
+                    for element in zone:
+                        if element != '': condition_3 = False
+                    
+                    if condition_1 and condition_2 and condition_3:
+                        self.boats.iloc[bow: stern: way, course] = 'O'
+                        boat_in = True
+
+                    else:
+                        attemps +=1
+                        continue
+                    
+                if way == 'S' or way == 'W':
+                    stern = bow - ship
+                    way = -1
+                    zone = self.boats.iloc[course, bow: stern: way]
+                    
+                    # Check if zone inside ocean:
+                    condition_1 = bow in range(self.shots.shape[0])
+                    condition_2 = stern in range(self.shots.shape[0])
+                    
+                    # Check if zone is empty:
+                    condition_3 = True
+                    for element in zone:
+                        if element != '': condition_3 = False
+                    
+                    if condition_1 and condition_2 and condition_3:
+                        self.boats.iloc[bow: stern: way, course] = 'O'
+                        boat_in = True
+
+                    else:
+                        attemps +=1
+                        continue
+                        
+        for row in range(self.boats.shape[0]):
+            for col in range(self.boats.shape[1]):
+                if self.boats.iloc[row, col] == 'O':
+                    coordinates.append((ocean[1][col], row + 1))
+                    
+        return coordinates
+    
     def clear(self):
         self.boats.loc[:, :] = ''
         self.shots.loc[:, :] = ''
 
 
 
-# random_boats()
+# GENERATE FLEET
+
+def fleet():
+        fleet = input('\n\nIntroduce la flota con la que vamos a jugar y pulsa ENTER:\n' + 
+                    'Deberás introducir la eslora de cada barco, separada por comas.\n' +
+                    '\nPor ejemplo: 1,1,1,1,2,2,2,3,3,4 creará:\n\n' +
+                    'Cuatro barcos de eslora 1\n' +
+                    'Tres barcos de eslora 2\n' +
+                    'Dos barcos de eslora 3\n' +
+                    'Un barco de eslora 4\n')
+
+        fleet = fleet.strip().replace(' ', '').split(sep = ',')
+        fleet = list(map(int, fleet))
+
+        return fleet
 
 
 
